5b200dae9a498c888feb1a9cb7dd9ea9d515c62f
cthululululu <124101856+cthululululu@users.noreply.github.com>
2024-04-28 13:25:21 -0400

    Made the copyDirectory and copyFile method

--- Directory1/CreateFilDir.java
+++ Directory1/CreateFilDir.java
@@ -61,4 +61,34 @@ public class CreateFilDir {
 	  public void pwd() {
 			  System.out.println("Working Directory = " + System.getProperty("user.dir"));
 	  }
+
+	public static void copyDirectory (File sourceDirectory, File destinationDirectory) throws IOException {
+        if(!destinationDirectory.exists()){ //checks that destination directory exists, if it doesn't a new directory is created.
+            destinationDirectory.mkdir();
+        }
+        for(String s : sourceDirectory.list()){ //for-each loop to iterate over the sourceDirectory 
+            //sourceDirectory.list() returns an array of Strings that names the files and directories in the original directory
+            copyDirectoryMode(new File(sourceDirectory, s), new File(destinationDirectory, s)); //calls copyDirectoryMode method to copy the Directory
+       		 }
+    	}
+    	public static void copyDirectoryMode(File source, File dest) throws IOException{ //determines if given source is a file or a directory
+        	if(source.isDirectory()){
+            copyDirectory(source, dest); //if directory iteratively calls copyDirectory method
+        		}
+        	else{
+            copyFile(source,dest); //if file calls the copyFile method
+        		}
+    } 
+    
+    	public static void copyFile(File sourceFile, File destFile) throws IOException{
+        try(InputStream in = new FileInputStream(sourceFile); OutputStream out = new FileOutputStream(destFile))  //declaring the resources
+        // try-with resources statement. Resource is an object that must be closed after program is finished with it. Prevents waste.
+        { //reading from in and writing to out
+            byte [] buffer = new byte[1024]; //declaring a byte array of size 1024 
+            int length;
+            while((length = in.read(buffer)) > 0 ){  //read method of inputStream is called and returns number of bytes read if no bytes read (-1) it terminates the loop bc we have reached the end of the stream.
+                out.write(buffer,0,length); //read data is then written to the outputstream using write method. 
+            }
+        }
+    }
 }
